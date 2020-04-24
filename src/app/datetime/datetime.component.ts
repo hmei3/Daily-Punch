55fb87d66d91e21eb21420d806a87a80f1fa0074
Hao Mei <haomei2387@gmail.com>
2020-04-23 23:59:30 -0500

    complete datetime component

--- src/app/datetime/datetime.component.ts
+++ src/app/datetime/datetime.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { timer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-datetime',
   templateUrl: './datetime.component.html',
   styleUrls: ['./datetime.component.scss']
 })
-export class DatetimeComponent implements OnInit {
+export class DatetimeComponent implements OnInit, OnDestroy {
 
+  private dateSub: Subscription;
+  today = Date.now();
   constructor() { }
 
   ngOnInit(): void {
+    const numbers = timer(0, 1000);
+    this.dateSub = numbers.subscribe(() => {
+      this.today = Date.now();
+    });
+  }
+  ngOnDestroy(): void {
+    this.dateSub.unsubscribe();
   }
 
 }
